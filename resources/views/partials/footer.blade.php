0d23128efb06ffe369f3909329cba1bbf7c8dc7a
이동영 <danik@idong-yeong-ui-MacBookPro.local>
2022-02-08 16:57:48 +0900

    bladefinished

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,12 @@
 <footer>
     <p>저는 꼬리말입니다. 다른 뷰에서 저를 입양해 가요. 어디든지 꼽사리낄 수 있죠</p>
 </footer>
+
+@section('script')
+
+    <script>
+        alert("저는 조각 뷰의 'script' 섹션입니다. 완투쓰리")
+    </script>
+    @parent
+
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,7 @@
     <script>
         alert("저는 자식 뷰의 'script' 섹션입니다.");
     </script>
+@endsection
 
 <ul>
     @forelse($items as $item)
