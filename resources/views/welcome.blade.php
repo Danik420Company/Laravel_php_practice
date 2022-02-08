375ce9cc9f97023498eb7f17379c704710d02859
이동영 <danik@idong-yeong-ui-MacBookPro.local>
2022-02-08 15:58:47 +0900

    blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,24 @@
-<h1><?= isset($greeting) ? "{$greeting} " : 'Hello '; ?><?= $name; ?></h1>
+@extends('layouts.master')
+
+@section('style')
+    <style>
+        body {background: green; color: white;}
+    </style>
+@endsection
+
+@section('content')
+    <p>저는 자식 뷰의 'content' 섹션입니다.</p>
+@endsection
+
+@section('script')
+    <script>
+        alert("저는 자식 뷰의 'script' 섹션입니다.");
+    </script>
+
+<ul>
+    @forelse($items as $item)
+        <li>{{ $item }}</li>
+    @empty
+        <li>엥~ 아무것도 없는데요!</li>
+    @endforelse
+</ul>
